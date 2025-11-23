880f19076809ac20253bac8485f3809f3ef1a9bf
maximo12345678901 <maximozweers@gmail.com>
2025-11-23 18:14:55 +0100

    Modified the .gitignore, and something else

--- Mandelbrot-set/main.cpp
+++ Mandelbrot-set/main.cpp
@@ -44,6 +44,42 @@ int main() {
 
     int frameCount = 1;
 
+
+    sf::Image image;
+    image.create(windowSize.x, windowSize.y, sf::Color::Black);
+    #pragma omp parallel for collapse(2)
+    for (int x = 0; x < windowSize.x; x++) {
+        for (int y = 0; y < windowSize.y; y++) {
+            sf::Vector2f c(0.0, 0.0);
+            sf::Vector2f z(0.0, 0.0);
+
+            c.x = xMin + (xMax - xMin) * x / windowSize.x;
+            c.y = yMin + (yMax - yMin) * y / windowSize.y;
+
+
+            for (int i = 0; i < maxIterations; ++i) {
+                sf::Vector2f power = complexPow(z, exponent);
+
+                z.x = power.x + c.x;
+                z.y = power.y + c.y;
+
+                if (z.x * z.x + z.y * z.y > 4.0) {
+                    float t = static_cast<float>(i) / maxIterations;
+                    float colorValue = std::sqrt(t) * 255.0f;
+                    sf::Uint8 gray = static_cast<sf::Uint8>(colorValue);
+                    image.setPixel(x, y, sf::Color(gray, gray, gray));
+                    break;
+                }
+            }
+        }
+    }
+
+    sf::Texture texture;
+    texture.loadFromImage(image);
+
+    sf::Sprite sprite(texture);
+
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -52,48 +88,15 @@ int main() {
             }
         }
 
-        sf::Image image;
-        image.create(windowSize.x, windowSize.y, sf::Color::Black);
-        #pragma omp parallel for collapse(2)
-        for (int x = 0; x < windowSize.x; x++) {
-            for (int y = 0; y < windowSize.y; y++) {
-                sf::Vector2f c(0.0, 0.0);
-                sf::Vector2f z(0.0, 0.0);
+        // std::ostringstream filenameStream;
+        // filenameStream << "frames/frame_" << std::setw(4) << std::setfill('0') << frameCount << ".png";
+        // std::string filename = filenameStream.str();
 
-                c.x = xMin + (xMax - xMin) * x / windowSize.x;
-                c.y = yMin + (yMax - yMin) * y / windowSize.y;
+        // image.saveToFile(filename);
 
-
-                for (int i = 0; i < maxIterations; ++i) {
-                    sf::Vector2f power = complexPow(z, exponent);
-
-                    z.x = power.x + c.x;
-                    z.y = power.y + c.y;
-
-                    if (z.x * z.x + z.y * z.y > 4.0) {
-                        float t = static_cast<float>(i) / maxIterations;
-                        float colorValue = std::sqrt(t) * 255.0f;
-                        sf::Uint8 gray = static_cast<sf::Uint8>(colorValue);
-                        image.setPixel(x, y, sf::Color(gray, gray, gray));
-                        break;
-                    }
-                }
-            }
-        }
-
-        sf::Texture texture;
-        texture.loadFromImage(image);
-
-        sf::Sprite sprite(texture);
-
-        std::ostringstream filenameStream;
-        filenameStream << "frames/frame_" << std::setw(4) << std::setfill('0') << frameCount << ".png";
-        std::string filename = filenameStream.str();
-
-        image.saveToFile(filename);
-
-        exponent += 0.01;
-        frameCount++;
+        // exponent += 0.01;
+        // frameCount++;
+        
         window.clear();
         window.draw(sprite);
         window.display();
